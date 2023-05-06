b9770f71802079070c722f9304812e1b3ea427e9
sumir <sumir40@gmail.com>
2023-05-06 19:39:07 +0530

    change 004 add ip system

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import MetaData
-
+from core.factory import get_ip
 # Create your views here.
 
 class HomeView(View):
@@ -11,10 +11,11 @@ class HomeView(View):
     def get(self, request):
         print(request.META)
         args = request.META
+        ip = get_ip(args)
         data = {
                 "server_name": args.get('SERVER_NAME'),
                 "http_host":args.get('HTTP_HOST'),
-                "remote_addr":args.get('REMOTE_ADDR'),
+                "remote_addr":ip,
                 "user":args.get('USERNAME'),
                 }
         obj = MetaData.objects.create(**data)
